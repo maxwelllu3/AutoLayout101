c506db77e034035bc3cd1f029c06b373a3a24167
Russell Gordon <github@russellgordon.ca>
2019-10-30 15:43:31 -0400

    Initial Commit

---
+++ Auto Layout 101/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout 101
+//
+//  Created by Andrei Puni on 02/09/15.
+//  Copyright © 2015 Andrei Puni. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
